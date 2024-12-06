9c41dfdc634cac60c3af8e20f49869bd6c6d6186
Daner SHARIFI <daner.sharifi.official@gmail.com>
2024-12-06 04:38:55 +0100

    login page

--- login.php
+++ login.php
@@ -7,7 +7,49 @@ $metaTitle = 'TP5 Dev. Web by Daner SHARIFI';
 
 ob_start();
 ?>
-    <h2 class="text-center my-5">Bienvenue dans l'espace sécurisé du site !</h2>
+    <br><br><br>
+    <div class="login-area ptb-100">
+        <div class="container">
+            <div class="login-form">
+                <div class="login-title">
+                    <i class="far fa-user text-center"></i>
+                    <h3>Connectez vous</h3>
+                </div>
+                <div class="form-sing">
+                    <form>
+                        <div class="row justify-content-center">
+                            <div class="col-lg-12 col-md-12">
+                                <div class="form-group">
+                                    <label for="username">Username</label>
+                                    <input id="username" type="text" class="form-control" placeholder="Username">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12 col-md-12">
+                                <div class="form-group">
+                                    <label for="password">Mot de passe</label>
+                                    <input id="password" type="password" class="form-control" placeholder="Password">
+                                </div>
+                            </div>
+                            <div class="col-lg-12 col-md-12">
+                                <div class="form-check form-group">
+                                    <input type="checkbox" class="form-check-input" id="remember">
+                                    <label class="form-check-label" for="remember">Se souvenir de moi</label>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12 col-md-12 text-center">
+                                <button type="submit" class="login-btn btn-two">
+                                    Log In
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <p>Don't have an account? <a href="register.html">Resigter Now</a></p>
+            </div>
+        </div>
+    </div>
 <?php
 $pContent = ob_get_clean();
 
