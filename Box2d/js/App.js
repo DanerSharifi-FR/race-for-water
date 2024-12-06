2262fdb8308aad063f8d4f02f15bbbeb0f47b65b
Titouan Delaplagne <161587036+Bob-the-great-the-third@users.noreply.github.com>
2024-12-06 06:57:30 +0100

    GD Full
    Slight bug on the collisions

--- Box2d/js/App.js
+++ Box2d/js/App.js
@@ -1,142 +1,220 @@
-(function () {
-    var b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
-    var b2Vec2 = Box2D.Common.Math.b2Vec2;
-    var box2dUtils;        // Classe utilitaire
-    var world;             // "Monde" 2D Box2D
-    var canvas;            // Canvas HTML
-    var canvasWidth;       // Largeur du canvas
-    var canvasHeight;      // Hauteur du canvas
-    var context;           // Contexte 2D du canvas
+document.addEventListener('DOMContentLoaded', (event) => {
 
-    var posX;              // Position X du sol
-    var posY;              // Position Y du sol
-    var lastPosX = 0;      // Dernière position X pour générer le sol
-    var lastPosY = 0;      // Dernière position Y pour générer le sol
+    const tileSize = 64;
+    var canva = document.getElementById('gipCanvas');
+    canva.width = tileSize * 12;
+    canva.height = tileSize * 8;
+    const ctx = canva.getContext('2d');
 
-    // Initialisation
-    $(document).ready(function () {
-        init();
+    class Tile {
+        constructor(x, y, type) {
+            this.x = x;
+            this.y = y;
+            this.type = type;
+        }
+
+        renderTile(xOffset, yOffset) {
+            switch (this.type) {
+                case 'pike':
+                    ctx.beginPath();
+                    ctx.moveTo((this.x) * tileSize + xOffset + tileSize / 2, this.y * tileSize + yOffset); // Top point
+                    ctx.lineTo((this.x) * tileSize + xOffset, this.y * tileSize + yOffset + tileSize); // Bottom left
+                    ctx.lineTo((this.x) * tileSize + xOffset + tileSize, this.y * tileSize + yOffset + tileSize); // Bottom right
+                    ctx.closePath();
+                    ctx.fillStyle = 'darkred'; // color for triangular pikes
+                    ctx.fill();
+                    ctx.strokeStyle = 'blue'; // border color for triangular pikes
+                    ctx.stroke();
+                    break;
+                case 'block':
+                    ctx.fillStyle = 'brown'; // color for squary blocks
+                    ctx.fillRect((this.x) * tileSize + xOffset, (this.y) * tileSize + yOffset, tileSize, tileSize);
+                    ctx.strokeStyle = 'black'; // color for the edge
+                    ctx.strokeRect((this.x) * tileSize + xOffset, (this.y) * tileSize + yOffset, tileSize, tileSize);
+                    break;
+                case 'air':
+                    // Air might be transparent, so no fill required
+                    ctx.clearRect((this.x + xOffset) * tileSize, (this.y) * tileSize + yOffset, tileSize, tileSize);
+                    break;
+                default:
+                    console.error('Unknown tile type');
+
+            }
+        }
+    }
+
+    class Map {
+        constructor() {
+            this.x = 0;
+            this.y = 0;
+            this.map = [];
+            for (let y = 0; y < 8; y++) {
+                for (let x = 0; x < 13; x++) {
+                    let type = (y < 6) ? 'air' : 'block';
+                    this.map.push(new Tile(x, y, type));
+                }
+            }
+
+        }
+
+        render() {
+            this.map.forEach(tile => {
+                tile.renderTile(this.x, this.y);
+            });
+        }
+
+        update() {
+            this.x -= 4; // Moving the tiles more to the left to create a quicker motion effect
+            if (this.x <= -tileSize) {
+                this.genColomn();
+                this.x += tileSize;
+            }
+        }
+
+        genColomn() {
+            this.map = this.map.filter(tile => tile.x > 0);
+            this.map.forEach(tile => tile.x--);
+
+            // Add new column of tiles on the right
+            const newColumnX = this.map.reduce((max, tile) => Math.max(max, tile.x), -1) + 1;
+            for (let y = 0; y < 8; y++) {
+                let type = (y < 6) ? 'air' : 'block';
+                if(this.fineToPut(newColumnX) && y===5 && Math.random()<0.8){
+                    type = 'pike';
+                }
+                this.map.push(new Tile(newColumnX, y, type));
+            }
+        }
+
+        fineToPut(newColumnX){
+
+            const pikeCount = this.map.filter(tile => tile.y === 5 && tile.type === 'pike').length;
+            const lastColumnWithPike = this.map.filter(tile => tile.type === 'pike').reduce((maxX, tile) => Math.max(maxX, tile.x), -1);
+
+
+
+            var istGoot= pikeCount < 3 && (lastColumnWithPike === (newColumnX - 1) || pikeCount === 0);
+            if(istGoot && pikeCount === 0)
+                cpt++;
+
+            return istGoot;
+        }
+
+    }
+
+    class Player {
+        constructor() {
+            this.x = 100;
+            this.y = 200;
+            this.jumping = false;
+            this.falling = false;
+            this.gravity = 0.4; // pixels per frame
+            this.spacePressed = false;
+        }
+
+        render() {
+            ctx.fillStyle = 'transparent';
+            ctx.fillRect(this.x, this.y, 64, 64);
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect(this.x, this.y, 64, 64);
+        }
+
+        update(map) {
+            this.yVelocity = this.yVelocity || 0;
+
+            // Adjust gravity when space is pressed during fall
+            const effectiveGravity = (this.spacePressed) ? (this.gravity / 2) : this.gravity;
+
+            // Apply gravity to velocity
+            this.yVelocity += effectiveGravity;
+
+            // Update player position based on velocity
+            this.y += this.yVelocity;
+
+            // Check if the player hits a block or pike on the map
+            for (let tile of map.map) {
+                if (this.collidesWith(tile, tileSize)) {
+                    if (tile.type === 'block') {
+                        if (this.yVelocity > 0) { // Falling
+                            this.y = tile.y * tileSize - 64; // Align the player on top of the block
+                            this.falling = false;
+                            this.jumping = false;
+                            this.yVelocity = 0;
+                        }
+                    } else if (tile.type === 'pike') {
+                        defeat();
+                        break;
+                    }
+                }
+            }
+        }
+
+        collidesWith(tile, tileSize) {
+            let pX1 = this.x, pY1 = this.y,
+                pX2 = this.x +  tileSize, pY2 = this.y + tileSize;
+            let tX1 = tile.x * tileSize, tY1 = tile.y * tileSize,
+                tX2 = tile.x * tileSize + tileSize, tY2 = tile.y * tileSize + tileSize;
+
+            return !(pX2 <= tX1 || pX1 >= tX2 || pY2 <= tY1 || pY1 >= tY2);
+        }
+
+        jump() {
+            if (!this.jumping && !this.falling) {
+                this.jumping = true;
+                this.yVelocity = -10; // Initial jump velocity
+            }
+        }
+    }
+
+    var player = new Player();
+
+    var map = new Map();
+    map.render();
+    player.render();
+
+    var cpt=0;
+
+    function gameLoop() {
+        // Clear the canvas
+        ctx.clearRect(0, 0, canva.width, canva.height);
+
+        // Render the map and the player
+        map.update();
+        map.render();
+        player.update(map); // Ensure player update is called before render
+        player.render();
+
+        if(cpt>20)
+            win();
+        // Call the gameLoop again on the next animation frame
+        requestAnimationFrame(gameLoop);
+    }
+
+    function win(){
+        console.log("win");
+    }
+
+    function defeat(){
+        console.log("defeat");
+    }
+
+// Start the game loop
+    gameLoop();
+
+
+    document.addEventListener('keydown', event => {
+        if (event.code === 'Space') {
+            player.jump();
+            player.spacePressed = true;
+        }
     });
 
-    // Lancer à l'initialisation de la page
-    this.init = function () {
-
-        box2dUtils = new Box2dUtils();	// instancier la classe utilitaire
-
-        // Récupérer la canvas, ses propriétés et le contexte 2d
-        canvas = $('#gipCanvas').get(0);
-        canvasWidth = parseInt(canvas.width);
-        canvasHeight = parseInt(canvas.height);
-        context = canvas.getContext('2d');
-
-        world = box2dUtils.createWorld(context); // box2DWorld
-
-        // Créer le "sol" de notre environnement physique
-        //ground= box2dUtils.createBox(world, canvasWidth / 2, canvasHeight - 10, canvasWidth / 2, 10, true, 'ground');
-
-        var car = box2dUtils.createPolygon(
-            world,
-            200, // Position X
-            200, // Position Y
-            false, // Objet dynamique
-            'customPolygon', // User data
-            [
-                {x: 50, y: -20},
-                {x: 50, y: 20},
-                {x: -60, y: 20},
-                {x: -60, y: -20}
-            ]
-        );
-
-        box2dUtils.addPolygon(world,
-            200, // Position X
-            200, // Position Y
-            false, // Objet dynamique
-            'customPolygon',
-            car,// User data
-            [
-                {x: 42.42640687119285, y: 0 - 20},
-                {x: 35.35533905932738, y: 7.0710678118654755 - 20},
-                {x: 0, y: -21.213203435596427 - 20},
-                {x: 7.0710678118654755, y: -28.284271247461902 - 20}
-            ]
-        )
-
-        box2dUtils.addPolygon(world, 200, 200, false, 'dudeBody', car,
-            [
-                {x: -20, y: -40},
-                {x: -20, y: 10},
-                {x: -10, y: 10},
-                {x: -10, y: -40}
-            ])
-
-        rightTire = box2dUtils.createBall(world, 265, 240, 10, false, 'customBall');
-
-        leftTire = box2dUtils.createBall(world, 160, 240, 10, false, 'customBall');
-
-        var prismaticJointDefRight = new Box2D.Dynamics.Joints.b2PrismaticJointDef();
-        prismaticJointDefRight.Initialize(car, rightTire, rightTire.GetWorldCenter(), new Box2D.Common.Math.b2Vec2(0, 1));
-        prismaticJointDefRight.lowerTranslation = -0.5;
-        prismaticJointDefRight.upperTranslation = 0.5;
-        prismaticJointDefRight.enableLimit = true;
-
-        var prismaticJointRight = world.CreateJoint(prismaticJointDefRight);
-
-        // Créer le sol initial
-        ground = box2dUtils.the_ground(world, canvasWidth / 2, canvasHeight - 10, canvasWidth / 2, 70, true);
-
-        // Démarrer la mise à jour du jeu
-        setInterval(gameUpdate, 1000 / 30); // 30 FPS
-
-        // Exécuter le rendu de l'environnement 2D
-        window.setInterval(update, 1000 / 60);
-    }
-
-
-        // Mettre à jour le rendu de l'environnement 2D
-        this.update = function () {
-            // Effectuer les simulations physiques et mettre à jour le canvas
-            world.Step(1 / 60, 10, 10);
-            world.DrawDebugData();
-            world.ClearForces();
+    document.addEventListener('keyup', event => {
+        if (event.code === 'Space') {
+            player.spacePressed = false;
         }
+    });
+
+});
 
 
-
-    this.gameUpdate = function () {
-        // Paramètres
-        var segmentLength = 100, pointCount = 5;
-
-        // Créer un tableau de points pour chaque segment
-        var segment = [];
-        for (var i = 0; i < pointCount; i++) {
-            var newX = lastPosX + (i * (segmentLength / (pointCount - 1))); // Position X
-            var newY = lastPosY + (Math.random() * 400 - 20); // Variation Y aléatoire
-            segment.push({ x: newX, y: newY });
-        }
-
-        // Fermer le segment avec les points bas du sol
-        segment.unshift({ x: segment[0].x, y: canvasHeight - 10 });
-        segment.push({ x: segment[segment.length - 1].x, y: canvasHeight - 10 });
-
-        // Ajouter ce segment de terrain au monde Box2D
-        box2dUtils.addPolygon(world, lastPosX, lastPosY, true, 'groundSegment', ground, segment);
-
-        //box = box2dUtils.createBox( world, lastPosX, lastPosY, 40, 50, true, "box");
-
-        // Mettre à jour les positions pour le prochain segment
-        lastPosX = segment[segment.length - 1].x;
-        lastPosY = segment[segment.length - 1].y;
-
-    }
-
-
-
-
-
-
-
-
-
-
-}());
